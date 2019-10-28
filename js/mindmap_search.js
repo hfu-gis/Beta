eafca7dec744aeb6ee4bdf423a8b3b7d53773e6f
raphaelhild <raphael.d.hild@hs-furtwangen.de>
2019-10-28 21:22:55 +0100

    - Ausprobieren der Merge-Funktion - Erste Versuche mit JS und Canvas - Anfang Mindmap

--- js/mindmap_search.js
+++ js/mindmap_search.js
@@ -1,26 +1,25 @@
 function draw() {
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
+        var e = document.getElementById("meinKreisCanvas");
+        var canv = e.getContext("2d");
+        canv.fillStyle = "black";
+        canv.arc(150, 150, 100, 0, 2 * Math.PI);
+        canv.fill();
 
-        for (var i = 0; i < 4; i++) {
-            for (var j = 0; j < 3; j++) {
-                ctx.beginPath();
-                var x = 25 + j * 50; // x coordinate
-                var y = 25 + i * 50; // y coordinate
-                var radius = 20; // Arc radius
-                var startAngle = 0; // Starting point on circle
-                var endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
-                var anticlockwise = i % 2 != 0; // clockwise or anticlockwise
 
-                ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+        var e = document.getElementById("meinKreisCanvas2");
+        var canv2 = e.getContext("2d");
+        canv2.fillStyle = "black";
+        canv2.arc(50, 150, 50, 0, 2 * Math.PI);
+        canv2.fill();
 
-                if (i > 1) {
-                    ctx.fill();
-                } else {
-                    ctx.stroke();
-                }
-            }
-        }
-    }
+    var e = document.getElementById("Linie");
+    var canv2 = e.getContext("2d");
+    canv2.fillStyle = "black";
+    canv2.beginPath();
+    canv2.moveTo(75, 50);
+    canv2.moveTo(900,900);
+    canv2.stroke();
+
+
 }
+
