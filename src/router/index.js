9b9eb1b18fa533a049f134d1c978bbe2683de95d
SebiPf <Sebastian.Pfeiffer@hs-Furtwangen.de>
2020-01-11 15:33:39 +0100

    seiten hinzugefügt

--- src/router/index.js
+++ src/router/index.js
@@ -54,6 +54,6 @@ export default new VueRouter({
         {
             path: '/searchBlank',
             component: searchBlank
-        }
+        },
     ]
 })
---
+++ src/views/searchBlank.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="body-1">
+    <!-- HTML-Code aus dem Body-Tag hier einfügen -->
+  </div>
+</template>
+
+<script>
+export default {
+  // gebt jeder Page einen eigenen Namen
+  name: 'searchBlank',
+
+  // benötigte Komponenten
+  components: {},
+
+  // entspricht den HTML-Attributen
+  props: {},
+
+  // Variablen-Speicher
+  data() {
+    return {}
+  },
+
+  // reagieren auf prop-Veränderung
+  watch: {},
+
+  // interne Methoden
+  methods: [],
+
+  // Initialisierung
+  created() {
+    this.$router.replace('/postinglist')
+  }
+}
+</script>
+
+<style scoped>
+/* CSS für diese Seite hier einfügen */
+</style>
