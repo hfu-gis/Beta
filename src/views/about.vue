ced091956dbed5916ecfb1ca1fa71d9fddef3047
Fokus <romtham.gebhardt@hs-furtwangen.de>
2020-01-12 15:11:39 +0100

    about us web fertig!

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="body-1">
-    <!-- HTML-Code aus dem Body-Tag hier einfügen -->
+
+    <v-carousel height="51em" hide-delimiter-background show-arrows-on-hover>
+      <v-carousel-item v-for="(slide, i) in slides" :key="i">
+        <v-sheet :color="colors[i]" height="100%">
+          <v-row class="fill-height" align="center" justify="center">
+            <div class="display-1"><h1 style="margin-bottom: 1.5em; margin-left: 1em; font-size: 1em; font-family: Standard-Font">{{ slide.h1}}</h1>
+              <p style=" margin: 1.5em;font-size: 0.85em; font-family: Standard-Font" align="left" justify="center">{{ slide.text}}</p>
+
+            </div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+
   </div>
 </template>
 
@@ -16,8 +29,24 @@ export default {
   props: {},
 
   // Variablen-Speicher
-  data() {
-    return {}
+  data () {
+    return {
+      colors: [
+        '#8C3F63',
+        '#BF4974',
+        '#8C3F63',
+      ],
+      slides: [
+        {h1:'about us',text:'Welcome to Bit of advice, your number one source for all things. We\'re dedicated to giving you the very best of advice, with a focus on accuracy, efficient,simplicity.\n' +
+                  '\n' +'\n' +'Founded in 2019 by team Beta, Bit of advice has come a long way from its beginnings in Furtwangen. When we first started out, our passion for idea of our website drove them to do tons of research, so that Bit of advice can offer you the best advice. We now serve customers all over the world, and are thrilled that we\'re able to turn our passion into our own website.\n' +
+                  '\n' +'\n' + 'We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don\'t hesitate to contact us.\n' +
+                  '\n' +'\n' +'Sincerely,\n' + '\n' +
+                  'Team Beta'},
+        {h1:'what is Bit of advice',text:'Bit of advice is home to thousands of communities, endless conversation, and authentic human connection. Whether you\'re into breaking news, sports, TV fan theories, or a never-ending stream of the internet\'s cutest animals, there\'s a community on Bit of advice for you.'},
+        {h1:'How Does Reddit Work?',text:'Every day, millions of people around the world post, vote, and comment in communities organized around their interests.'},
+
+      ],
+    }
   },
 
   // reagieren auf prop-Veränderung
